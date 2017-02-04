f5891331bac44e276a8ac89caa4d9b1c9334148a
cdcvidal <cdcvidal@yahoo.fr>
2017-02-04 09:56:12 +0100

    Add HammerJS and Material design icons

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,8 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
+import 'hammerjs';
+
 import { AppComponent } from './app.component';
 
 @NgModule({
